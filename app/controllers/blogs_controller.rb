3e3c039201820300afd89baa925370076167e335
yuukitakei <consultation.pharmacist3@gmail.com>
2019-04-15 00:10:18 +0900

    createアクション追加。ここはparamsの大事な考え方なのでしっかり理解すること

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,4 +5,12 @@ class BlogsController < ApplicationController
   def new
     @blog = Blog.new
   end
+
+  def create
+    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    # paramsはターミナルのparameterの内容
+    # Parameters: {"utf8"=>"✓", "authenticity_token"=>"pjm3hUIIYcYC4DZg6GseBXMfyywD3+WiKCODKoD3Hm8G9C3P69Lcv9ikUeoBHDT0AptywoAo+aG4Chh3ARtC0w==",
+    # "blog"=>{"title"=>"hoge", "content"=>"huga"}, "commit"=>"Create Blog"}
+    # ここのアクション内容は中身を取り出す為に行うコード
+  end
 end
