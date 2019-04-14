0b6477b6f3a631fc7a00206463212fd9afdbd381
yuukitakei <consultation.pharmacist3@gmail.com>
2019-04-15 02:27:47 +0900

    createでは直でテーブルに保存されてしまう為、バリデーションが許可した場合だけテーブルに保存させるという条件分岐を作成。 まず、ストロングパラメータにより、送られてくるパラメータの内容はtitleとcontentだけに絞ってある。 これを仮保存状態(new)にして、viewで見れるようにインスタンス変数に入れた。 このときのプログラムの流れは以下のようになってる（saveメソッドが走る瞬間にバリデーションが実行されている！）。 →saveはモデルメソッドであり、Blogモデルに対して、Blogテーブルに記録しろっていう命令をだすものなので、その命令がモデルに向かった際にモデルに書かれたバリデーションが働く、という流れ。 @blog.saveがバリデーションに許された（成功した）時 => @blog.saveの返り値はtrueとなり、redirect_to blogs_path, notice: "ブログを作成しました！"が実行される @blog.saveバリデーションに許されなかった（失敗した）時  => @blog.saveの返り値はfalseとなり、 render 'new' が実行される

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -13,10 +13,16 @@ class BlogsController < ApplicationController
     # Parameters: {"utf8"=>"✓", "authenticity_token"=>"pjm3hUIIYcYC4DZg6GseBXMfyywD3+WiKCODKoD3Hm8G9C3P69Lcv9ikUeoBHDT0AptywoAo+aG4Chh3ARtC0w==",
     # "blog"=>{"title"=>"hoge", "content"=>"huga"}, "commit"=>"Create Blog"}
     # ここのアクション内容は中身を取り出す為に行うコード
-     Blog.create(blog_params)
-     # 一番上の記述を上記に変えた（簡潔にした）
-    redirect_to new_blog_path
+     @Blog = Blog.new(blog_params)
+      # 一番上の記述を上記に変えた（簡潔にした）
+     if @blog.save
+    # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+    redirect_to new_blog_path,notice:"ブログを作成しました！"
+  else
+    # 入力フォームを再描画します。
+    render 'new'
   end
+end
 
   def show
     # 追記する
