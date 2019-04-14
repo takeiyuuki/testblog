c97d44bcbcfc370535ff8f61799e578d4f14e6dd
yuukitakei <consultation.pharmacist3@gmail.com>
2019-04-15 00:33:33 +0900

    (上段は)かっこの中を簡略化したコメントを書いた。 そのあとストロングパラメータを使用して、送られてくるパラメータを選択して、予期せぬ攻撃を防ぐようにした。 今回の場合だと、blogキーのtitle,contentのみ取得を許可します。ということ その後、リダイレクト先に飛ぶ。

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,10 +7,13 @@ class BlogsController < ApplicationController
   end
 
   def create
-    Blog.create(title: params[:blog][:title], content: params[:blog][:content])
+    # Blog.create(title: params[:blog][:title], content: params[:blog][:content])
     # paramsはターミナルのparameterの内容
     # Parameters: {"utf8"=>"✓", "authenticity_token"=>"pjm3hUIIYcYC4DZg6GseBXMfyywD3+WiKCODKoD3Hm8G9C3P69Lcv9ikUeoBHDT0AptywoAo+aG4Chh3ARtC0w==",
     # "blog"=>{"title"=>"hoge", "content"=>"huga"}, "commit"=>"Create Blog"}
     # ここのアクション内容は中身を取り出す為に行うコード
+     Blog.create(params.require(:blog).permit(:title, :content))
+     # 一番上の記述を上記に変えた（簡潔にした）
+    redirect_to new_blog_path
   end
 end
