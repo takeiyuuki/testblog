c3687d39c2c061739a93ed459d946ebc4df75984
yuukitakei <consultation.pharmacist3@gmail.com>
2019-04-16 02:18:12 +0900

    さっきも記載したやつぽい

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -17,11 +17,12 @@ class BlogsController < ApplicationController
     # Parameters: {"utf8"=>"✓", "authenticity_token"=>"pjm3hUIIYcYC4DZg6GseBXMfyywD3+WiKCODKoD3Hm8G9C3P69Lcv9ikUeoBHDT0AptywoAo+aG4Chh3ARtC0w==",
     # "blog"=>{"title"=>"hoge", "content"=>"huga"}, "commit"=>"Create Blog"}
     # ここのアクション内容は中身を取り出す為に行うコード
-     @Blog = Blog.new(blog_params)
+     @blog = Blog.new(blog_params)
       # 一番上の記述を上記に変えた（簡潔にした）
-      if @Blog.save
-    # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
-    redirect_to new_blog_path,notice:"ブログを作成しました！"
+      if @blog.save
+    # 一覧画面へ遷移して、"ブログを作成しました！"とメッセージを表示します。
+    # noticeオプションはredirect_toメソッドで使用できます。
+    redirect_to blogs_path, notice:"ブログを作成しました！"
       else
     # 入力フォームを再描画します。
     render 'new'
